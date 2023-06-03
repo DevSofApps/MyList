adb6a362b41b85dc0b18b640f798a157957ca8c4
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-06-03 11:42:42 -0300

    feat: :sparkles: atualizando brach

--- lib/app/core/config/app_colors.dart
+++ lib/app/core/config/app_colors.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color primary = Color(0XFFAD1814);
-  static const Color primaryLight = Color(0XFFDC332F);
+  static const Color primary = Color(0XFFF27649);
+  static const Color primaryLight = Color(0XFFF7D797);
   static const Color primaryDark = Color(0XFF710806);
-  static const Color primaryText = Colors.white;
-  static const Color hoverActive = Color(0XFFFEC82A);
+  static const Color primaryText = Color(0xFF2B2B2B);
+  static const Color secondaryText = Color(0xFF4B4B4B);
+  static const Color background = Color(0xFFE9E9E9);
+  static const Color green = Color(0XFF60AE68);
+  static const Color grey = Color(0XFF646464);
+  static const Color hoverGray = Color(0XFF747474);
+  static const Color white = Color(0XFFFFFFFF);
 }
--- lib/app/pages/auth/login/login_page.dart
+++ lib/app/pages/auth/login/login_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:my_list/app/core/widgets/textfield_widget.dart';
+
+import '../../../core/config/app_colors.dart';
+import '../../../core/services/validator.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,8 +12,51 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _formKey = GlobalKey<FormState>();
+  final emailTExtController = TextEditingController();
+  final passwordTExtController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  TextFieldWidget(
+                    icon: Icons.person,
+                    label: "Email",
+                    controller: emailTExtController,
+                    validator: emailValidator,
+                  ),
+                  TextFieldWidget(
+                    icon: Icons.person,
+                    label: "password",
+                    controller: passwordTExtController,
+                    validator: passwordValidator,
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColors.primary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                      ),
+                    ),
+                    onPressed: () => {},
+                    child: true
+                        ? const CircularProgressIndicator(
+                            backgroundColor: Colors.white)
+                        : const Text(
+                            'ENTRAR',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                  )
+                ],
+              ))
+        ],
+      ),
+    );
   }
 }
